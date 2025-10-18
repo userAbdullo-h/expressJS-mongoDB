4f998ae8410fd07b04b93f14ff964035b4a69df0
userAbdullo-h <abdullokhan3@gmail.com>
2025-10-18 23:07:13 +0500

    update auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,9 +1,10 @@
 function authMiddleware(req, res, next) {
-	if (req.session.user) {
-		return next()
+	if (!req.session.user) {
+		req.session.message = { typpe: 'danger', message: 'Please logging in.' }
+		return res.redirect('/auth/login')
 	}
 
-	res.redirect('/auth/login')
+	return next()
 }
 
 module.exports = authMiddleware
